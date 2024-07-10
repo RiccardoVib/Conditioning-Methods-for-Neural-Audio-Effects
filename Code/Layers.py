4f7f63b198ea7a284cb192f633f85b0159d21feb
RiccardoVib <riccardo.simionato.vib@gmail.com>
2024-07-10 13:31:31 +0200

    Update Layers.py

--- Code/Layers.py
+++ Code/Layers.py
@@ -53,39 +53,22 @@ class GLU(tf.keras.layers.Layer):
         gate = tf.keras.activations.softsign(gate)
         x = tf.multiply(out, gate)
         return x
+
+### from https://github.com/state-spaces/s4/blob/main/models/s4/
         
 class S4DKernel(tf.keras.layers.Layer):
-    """Generate convolution kernel from diagonal SSM parameters.
-        A: (S, N) diagonal matrix
-        B: (S, N)
-        C: (C, H, N)
-        dt: (H) timescale per feature
-
-        Dimensions:
-        N (or d_state): state size
-        H (or d_model): total SSM copies
-        S (or n_ssm): number of trainable copies of (A, B, dt); must divide H
-        C (or channels): system is 1-dim to C-dim
-
-        The forward pass of this Module returns a tensor of shape (C, H, L)
-
-    """
-
     def __init__(self,  N=64, d_model=1, dt_min=0.001, dt_max=0.1, b_size=600):
         super(S4DKernel, self).__init__()
         self.N = N
         # Generate dt
         self.H = d_model
-        #self.log_dt = tf.random.uniform([self.H]) * (math.log(dt_max) - math.log(dt_min)) + math.log(dt_min)
         self.log_dt = tf.random.uniform((self.H,), minval=tf.math.log(dt_min), maxval=tf.math.log(dt_max))
 
         #self.B = tf.Variable(0.5 * tf.ones((self.H, self.N//2)), trainable=True, dtype='float32')
-        #B = repeat(B, 't n -> (v t) n', v=self.n_ssm // B.size(-2)).clone().contiguous()
 
         C = tf.complex(tf.random.normal([self.H, self.N // 2]), tf.random.normal([self.H, self.N // 2]))
         self.C = tf.Variable(tf.cast(C, dtype=tf.float32), trainable=True)
 
-        #self.C = tf.Variable(tf.math.real(C), trainable=True, dtype='float32')
         self.log_dt = tf.Variable(self.log_dt, trainable=True)
 
         log_A_real = tf.math.log(tf.constant(0.5 * tf.ones((self.H, self.N//2))))
@@ -93,9 +76,6 @@ class S4DKernel(tf.keras.layers.Layer):
 
         self.log_A_real = tf.Variable(log_A_real, trainable=True)
         self.A_imag = tf.Variable(A_imag, trainable=True)
-
-        #self.log_A_real = tf.Variable(tf.math.log(0.5 * tf.ones((self.H, self.N//2))), trainable=False, dtype='float32')
-        #self.A_imag = tf.Variable(math.pi * repeat(np.arange(N//2), 'n -> h n', h=self.H), trainable=False, dtype='float32')
 
         self.b_size = b_size
         self.reset_states()
@@ -110,7 +90,6 @@ class S4DKernel(tf.keras.layers.Layer):
         C = tf.cast(self.C, dtype=tf.complex64)  # (H N)
 
         A = tf.dtypes.complex(-tf.exp(self.log_A_real), self.A_imag)
-        #A = -tf.exp(self.log_A_real) + 1j * self.A_imag  # (H N)
 
         # Vandermonde multiplication
         dt = tf.expand_dims(dt, axis=-1)
@@ -140,10 +119,9 @@ class S4DKernel(tf.keras.layers.Layer):
         self.state = tf.zeros((self.H, self.N//2), dtype=tf.complex64)
 
 class S4D(tf.keras.layers.Layer):
-    def __init__(self, d_state=64, d_model=1, transposed=True, **kernel_args):
+    def __init__(self, d_state=64, d_model=1, **kernel_args):
         super().__init__()
 
-        #self.T = T
         self.h = d_model
         self.n = d_state
         self.d_output = self.h
@@ -156,14 +134,10 @@ class S4D(tf.keras.layers.Layer):
 
         # Pointwise
         self.activation = tf.keras.activations.gelu
-
-        #self.conv = tf.keras.layers.Conv1D(2 * self.h, kernel_size=1, dtype='float32')
         self.glu = GLU(in_size=2*self.n, dim=-1)
 
     def call(self, u):  # absorbs return_output and transformer src mask
         """ Input and output shape (B, H, L) """
-        if not self.transposed:
-            u = tf.transpose(u, perm=[0, 2, 1])
         L = u.shape[-1]
 
         # Compute SSM Kernel
@@ -175,12 +149,6 @@ class S4D(tf.keras.layers.Layer):
 
         # Compute D term in state space equation - essentially a skip connection
         y = y + u * tf.expand_dims(self.D, axis=-1)
-
-        #y = self.activation(y)
-        #y = tf.expand_dims(y, axis=-1)
-        #y = self.conv(y)
         y = self.glu(y)
-        #self.conv_out
-        if not self.transposed:
-            y = tf.transpose(y, perm=[0, 2, 1])
+
         return y
